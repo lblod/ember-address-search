967815f3fff5f8a0990f3d510a88d86f20019330
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-02-27 11:10:29 +0100

    Drop Ember 3.28 support
    Ember v3 has been EOL for a very long time now, and all our apps are on v4.12+ now.

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -7,16 +7,6 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.28',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-            '@ember/test-helpers': '~2.9.3',
-            'ember-qunit': '~6.0.0',
-          },
-        },
-      },
-      {
         name: 'ember-lts-4.12',
         npm: {
           devDependencies: {
